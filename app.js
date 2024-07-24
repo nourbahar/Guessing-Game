bf3b3ef22bd30b3857a04efc3c023f76246fa558
Nour Bahar <nourbahar@gmail.com>
2024-07-24 15:14:57 +0300

    second

--- app.js
+++ app.js
@@ -3,37 +3,65 @@
 
 let computerChoice
 const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','S','Q','W','R','T','Y','U','I','O','P','Z','X','V','M','N']
-
+let chances=5
 
 
 /*------------------------ Cached Element References ------------------------*/
 let playerChoice 
 const input = document.querySelector('.text')
-const chances = document.querySelector('.chances')
+const chance = document.querySelector('.chances')
 const reset = document.querySelector('.resetbtn')
 const display = document.querySelector('.display')
 const check = document.querySelector(".check")
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+
 const play = () => {
      playerChoice = input.value.toUpperCase();
-    console.log(playerChoice);
-    getComputerChoice()
+    //console.log(playerChoice);
     compare()
   }
 
   const getComputerChoice = () => {
     const randomIndex = Math.floor(Math.random() * letters.length);
     computerChoice = letters[randomIndex];
+    //console.log(computerChoice)
+    
   };
 
   const compare = () => {
     if (playerChoice === computerChoice) {
       display.textContent = 'You Won!';
-    } else {
+      
+    } 
+    
+    else {
       display.textContent = 'Try again!';
-    }}
+        chances--;
+        chance.textContent= chances
+      }
 
+      if(chances === 0 ){
+        display.textContent = 'sorry you lost'
+        init()
+      }
+    }
+
+    
+    const init = () => {
+        display.textContent='';
+        chance.textContent = 5;
+        chances = 5;
+        input.value="";
+    }
+
+    getComputerChoice()
+    console.log(computerChoice)
+    
+    
 /*----------------------------- Event Listeners -----------------------------*/
 
-check.addEventListener('click', play);
+   check.addEventListener('click', play);
+   reset.addEventListener('click',init)
